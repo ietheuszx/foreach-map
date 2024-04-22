4dc162aa7b907b5963b1dd76f3c3b994696e7165
ietheuszx <fernandomaranss68@gmail.com>
2024-04-22 16:38:06 -0400

    adicionada questão13

---
+++ questao13.js
@@ -0,0 +1,7 @@
+/*
+Questão 13 - Quando você usaria map() em vez de forEach()?
+
+R: O map() é necessário quando for 
+   preciso criar um novo array no código, 
+   assim sem preicsar alterar o original
+*/
