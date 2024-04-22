5ef8ed04b6f1c91ff3315bff98a7b41d6805c0f4
ietheuszx <fernandomaranss68@gmail.com>
2024-04-22 16:39:03 -0400

    adicionada questão21

---
+++ questao21.js
@@ -0,0 +1,13 @@
+/*
+Questão 21 - Existe alguma diferença de desempenho entre map() e 
+forEach()?
+
+R: Sim, porém essa diferença é minima. Em 
+   geral, o map() é mais rápido que o 
+   forEach(), pois o mesmo só precisa 
+   imprimir um novo array com novos 
+   valores, enquanto o forEach() itera 
+   sobre o valor orignal e não retoma 
+   valor
+
+*/
