bd6cd437fdee180e2a3e9d04c652d6ac986a4093
ietheuszx <fernandomaranss68@gmail.com>
2024-04-22 16:37:40 -0400

    adicionada questão9

---
+++ questao9.js
@@ -0,0 +1,7 @@
+/*
+Questão 09 - Qual é a diferença fundamental entre map() e forEach()?
+
+R: map() é usado para criar um array com base no array original, utilizando a função de callback
+
+R: forEach() é usado para imprimir os dados do array original, sem altera-los
+*/
