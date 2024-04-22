00bbe6b0d934535cbb42e12a194221c51cc03d12
ietheuszx <fernandomaranss68@gmail.com>
2024-04-22 16:37:55 -0400

    adicionada questão11

---
+++ questao11.js
@@ -0,0 +1,5 @@
+/*
+Questão 11 - map() sempre retorna um novo array com o mesmo comprimento 
+que o array original?
+R: Sim, já que o map() usa o array original como base, assim imprimindo novos valores, porém, com a mesma quantidade de elementos
+*/
