36ecebb523e9ede098dcdf40fd740dc8bcf9fbe7
ietheuszx <fernandomaranss68@gmail.com>
2024-04-22 16:38:21 -0400

    adicionada questão16

---
+++ questao16.js
@@ -0,0 +1,7 @@
+/*
+Questão 16 - E se a função de callback fornecida ao método map() não 
+retornar nada? O que será retornado no novo array?
+
+R: Não será retornado nada, ou seja, o 
+   valor será "undefined"
+*/
