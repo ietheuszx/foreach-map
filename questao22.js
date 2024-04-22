16b54fde73c747772ec369ba7316fcb662623092
ietheuszx <fernandomaranss68@gmail.com>
2024-04-22 16:39:09 -0400

    adicionada questão22

---
+++ questao22.js
@@ -0,0 +1,10 @@
+/*
+Questão 22 - O que acontece se a função de callback passada para forEach() 
+modificar os elementos do array original?
+
+R: Quando o callback passa para o 
+  forEach(), ele acaba modificando o      
+  valor da array original, essa 
+  modificação pode ser refletida no array 
+  original
+*/
